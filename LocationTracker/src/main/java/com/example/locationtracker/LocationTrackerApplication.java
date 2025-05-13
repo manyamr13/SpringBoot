3060ef9490fd194c86287e928242f7edfd73225e
Manyam R <m.konda-c@takamol.com.sa>
2025-05-13 14:33:22 +0300

    locTcr

---
+++ LocationTracker/src/main/java/com/example/locationtracker/LocationTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.example.locationtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationTrackerApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(LocationTrackerApplication.class, args);
+    }
+}
