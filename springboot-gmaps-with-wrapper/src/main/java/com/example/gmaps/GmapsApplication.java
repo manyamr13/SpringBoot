81b2442a5eb41fc050c42c3795a588b7ad269504
Manyam R <m.konda-c@takamol.com.sa>
2025-05-14 11:48:46 +0300

    new

---
+++ springboot-gmaps-with-wrapper/src/main/java/com/example/gmaps/GmapsApplication.java
@@ -0,0 +1,11 @@
+package com.example.gmaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmapsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GmapsApplication.class, args);
+    }
+}
