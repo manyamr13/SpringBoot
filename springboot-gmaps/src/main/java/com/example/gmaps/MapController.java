7ca5a0b3a5eb3b7f8d98e19528e3c72174ab8e18
Manyam R <m.konda-c@takamol.com.sa>
2025-05-14 12:19:37 +0300

    updated controller

--- springboot-gmaps/src/main/java/com/example/gmaps/MapController.java
+++ springboot-gmaps/src/main/java/com/example/gmaps/MapController.java
@@ -1,10 +1,10 @@
 package com.example.gmaps;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class MapController {
 
     @GetMapping("/")
